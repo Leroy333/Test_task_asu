84ce59b17398022468111cdf12ba8a90ba1c99e5
Ruslan Dzhambekov <71041667+Leroy333@users.noreply.github.com>
2022-11-13 19:50:59 +0500

    C# Task

---
+++ TestProjectAsuNeft/TestProjectAsuNeft/Models/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿namespace TestProjectAsuNeft.Models;
+
+public class TreeNode : ITreeNode
+{
+    public int TypeObj { get; set ; }
+    public string NameObj { get ; set ; }
+    public List<ITreeNode> Childs { get; set; } = new();
+
+    public bool HasThisChild(int typeobj, string nameobj)
+    {
+        bool hasThisChild = false;
+
+        foreach (var item in Childs)
+            if (item.TypeObj == typeobj && item.NameObj== nameobj)
+                hasThisChild=true;
+
+        return hasThisChild;
+    }
+    public override string ToString()
+    {
+        return $"| Название - {NameObj,-10} | Тип - {TypeObj,-2} |";
+    }
+}
---
+++ TestProjectAsuNeft/TestProjectAsuNeft/Program.cs
@@ -0,0 +1,46 @@
+﻿TreeNode mainNode = new()
+{
+    NameObj = "Иван",
+    TypeObj = 0
+};
+TreeNode firstChildNode = new()
+{
+    NameObj = "Екатерина",
+    TypeObj = 1
+};
+TreeNode secondChildNode = new()
+{
+    NameObj = "Евгений",
+    TypeObj = 2
+};
+IList<TreeNode> treeNodes = new List<TreeNode>()
+{
+    mainNode, firstChildNode, secondChildNode
+};
+
+mainNode.Childs.Add(firstChildNode);
+mainNode.Childs.Add(secondChildNode);
+
+string separator = new string('-', 100);
+
+
+Console.WriteLine(separator);
+Console.WriteLine("Все объекты:");
+foreach (var item in treeNodes)
+    Console.WriteLine(item);
+Console.WriteLine(separator);
+
+Console.WriteLine($"Есть ли в объекте \"{mainNode.NameObj}\" " +
+    $"дочерний объект с названием \"{firstChildNode.NameObj}\" типом \"{firstChildNode.TypeObj}\" : " +
+    $"\"{mainNode.HasThisChild(firstChildNode.TypeObj, firstChildNode.NameObj)}\"");
+Console.WriteLine(separator);
+
+Console.WriteLine($"Есть ли в объекте \"{mainNode.NameObj}\" " +
+    $"дочерний объект с названием \"{secondChildNode.NameObj}\" типом \"{secondChildNode.TypeObj}\" : " +
+    $"\"{mainNode.HasThisChild(secondChildNode.TypeObj, secondChildNode.NameObj)}\"");
+Console.WriteLine(separator);
+
+Console.WriteLine($"Есть ли в объекте \"{mainNode.NameObj}\" " +
+    $"дочерний объект с названием \"{firstChildNode.NameObj}\" типом \"{secondChildNode.TypeObj}\" : " +
+    $"\"{mainNode.HasThisChild(secondChildNode.TypeObj, firstChildNode.NameObj)}\"");
+Console.WriteLine(separator);
